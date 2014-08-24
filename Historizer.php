efe67254ab689b14d829b136f0052879e0039b56
Borsos Albert <borsosalbert@Borsos-MacBook-Air.local>
2014-08-24 19:53:21 +0200

    initialize commit

---
+++ Historizer.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace albertborsos\yii2historizer;
+
+use albertborsos\yii2lib\db\ActiveRecord;
+use Yii;
+use yii\helpers\Json;
+
+/**
+ * This is the model class for table "ext_historizer_archives".
+ *
+ * @property integer $id
+ * @property string $model_class
+ * @property integer $model_id
+ * @property string $model_attributes
+ * @property integer $created_at
+ * @property integer $created_user
+ * @property integer $updated_at
+ * @property integer $updated_user
+ * @property string $status
+ */
+class Historizer extends ActiveRecord
+{
+    const STATUS_ACTIVE   = 'a';
+    const STATUS_INACTIVE = 'i';
+    const STATUS_DELETED  = 'd';
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'ext_historizer_archives';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['model_class', 'model_id', 'model_attributes', 'created_at', 'created_user', 'updated_at', 'updated_user', 'status'], 'required'],
+            [['model_id', 'created_at', 'created_user', 'updated_at', 'updated_user'], 'integer'],
+            [['model_attributes'], 'string'],
+            [['model_class'], 'string', 'max' => 512],
+            [['status'], 'string', 'max' => 1]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'model_class' => 'Model Class',
+            'model_id' => 'Model ID',
+            'model_attributes' => 'Model Attributes',
+            'created_at' => 'Created At',
+            'created_user' => 'Created User',
+            'updated_at' => 'Updated At',
+            'updated_user' => 'Updated User',
+            'status' => 'Status',
+        ];
+    }
+
+    public function beforeValidate()
+    {
+        if (parent::beforeValidate()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)){
+            $this->setOwnerAndTime();
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function beforeDelete()
+    {
+        if (parent::beforeDelete()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * @param \albertborsos\yii2lib\db\ActiveRecord $model
+     */
+    public static function createArchive($model){
+        $class = get_class($model); /** @var \albertborsos\yii2lib\db\ActiveRecord $class */
+
+        $oldModel = $class::findOne(['id' => $model->getPrimaryKey()]);
+        if (!self::attributesAreSame($model, $oldModel)){
+            $archive = new Historizer();
+            $archive->model_class      = $class;
+            $archive->model_id         = $model->getPrimaryKey();
+            $archive->model_attributes = Json::encode($oldModel->attributes);
+
+            if (!$archive->save()){
+                $archive->throwNewException('Archiválás nem sikerült!');
+            }
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * @param \albertborsos\yii2lib\db\ActiveRecord $model
+     * @param \albertborsos\yii2lib\db\ActiveRecord $oldModel
+     */
+    public static function attributesAreSame($model, $oldModel){
+        $newAttributes = $model->attributes;
+        $oldAttributes = $oldModel->attributes;
+        foreach($newAttributes as $id => $value){
+            if ($newAttributes[$id] !== $oldAttributes[$id]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,57 @@
+Yii2 Historizer
+===============
+Yii2 Model Historizer Extension
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist albertborsos/yii2-historizer "*"
+```
+
+or add
+
+```
+"albertborsos/yii2-historizer": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \albertborsos\yii2historizer\AutoloadExample::widget(); ?>```
+
+Init
+----
+
+```
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `ext_tbl_ahistorizer_histories`
+--
+
+CREATE TABLE IF NOT EXISTS `ext_historizer_archives` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `model_class` varchar(512) NOT NULL,
+  `model_id` int(11) NOT NULL,
+  `model_attributes` text NOT NULL,
+  `created_at` INT NOT NULL,
+  `created_user` INT NOT NULL,
+  `updated_at` INT NOT NULL,
+  `updated_user` INT NOT NULL,
+  `status` varchar(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+```
